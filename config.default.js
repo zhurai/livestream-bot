109a1c93f89d6671d80e1a402de80d0f8673661f
zhurai <zhurai.tsuki@gmail.com>
2022-07-21 17:43:21 -0700

    add more values for configuration

--- config.default.js
+++ config.default.js
@@ -1,8 +1,10 @@
 var config = {};
 config.username = "username";
-config.password = "password";
+config.password = "password"; // OAuth token / etc
 config.channels = ['channel'];
-
+config.refreshtoken = "twitch-refreshtoken";
+config.clientid = "twitch-clientid";
+config.clientsecret = "twitch-clientsecret";
 config.opts = {
     identity: {
       username: config.username,
