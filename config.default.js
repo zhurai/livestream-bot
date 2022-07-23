8ba3b5aded7fd5eb21e48081af0d6cb79a88df9a
zhurai <zhurai.tsuki@gmail.com>
2022-07-23 00:09:19 -0700

    update configuration file with notes

--- config.default.js
+++ config.default.js
@@ -1,8 +1,17 @@
 var config = {};
-config.username = "zhuraiChatBot";
-config.channels = ['channel'];
-config.refreshtoken = "twitch-refreshtoken";
+// Bot Username
+config.username = "BotUsername";
+// Channels the bot goes in
+config.channels = ['TheTwitchChannels'];
+
+// Twitch Application - Client ID: https://dev.twitch.tv/console
 config.clientid = "twitch-clientid";
+// Twitch Application - Client Secret: https://dev.twitch.tv/console
 config.clientsecret = "twitch-clientsecret";
+
+// Code - Log into the bot, and go to this URL: https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=CLIENTID&client_secret=CLIENTSECRET&scope=chat%3Aread+chat%3Aedit&state=c3ab8aa609ea11e793ae92361f002671&redirect_uri=http://localhost:3000
 config.code = "twitch-code";
+
+// Refresh Token  - run scripts/initial.js with the above inputs and copy it here
+config.refreshtoken = "twitch-refreshtoken";
 module.exports = { config };
