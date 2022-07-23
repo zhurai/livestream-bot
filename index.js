cddd772392afe212797a479b68341e843d14d985
zhurai <zhurai.tsuki@gmail.com>
2022-07-23 00:20:05 -0700

    complete rewrite of index.js

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const refresh = require('./refresh');
 
 opts = {
   identity: {
-    username: config.username,
-    password: 'oauth:'+refresh.get_accesstoken()
+    username: config.config.username,
+    password: 'oauth: TBD',
   },
-  channels: config.channels
+  channels: config.config.channels
 };
 
-// Create a client with our options
-const client = new tmi.client(opts);
+// Do not continue until all Promises have been fulfilled
+Promise.all([opts])
+.then(values => {
+  const client = tmi_client(values);
+  client.connect();
+})
+.catch(error => {
+  console.error(error.message);
+  process.exit(1)
+});
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-// Connect to Twitch:
-client.connect();
+// Creates and returns the tmi.js client
+function tmi_client(values){
+  var client = new tmi.client(values[0]);
+  client.on('message', onMessageHandler);
+  client.on('connected', onConnectedHandler);
+  return client;
+}
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
@@ -33,17 +42,12 @@ function onMessageHandler (target, context, msg, self) {
     client.say(target, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
   } 
-  else if (commandName === '!discord'){
-    const discordlink=discord()
-    client.say(target, `zhurai's Discord Link is ${discordlink}`);
-    console.log(`* Executed ${commandName} command`);
-  }
   else {
     console.log(`* Unknown command ${commandName}`);
   }
 }
 
-// Called every time the bot connects to Twitch chat
+// Called when connected
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
