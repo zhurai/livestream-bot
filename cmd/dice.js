6d12789a507e11df79bffd985b27501fbd0b7d96
zhurai <zhurai.tsuki@gmail.com>
2022-07-28 23:05:47 -0700

    fix: string literal to template literal

--- cmd/dice.js
+++ cmd/dice.js
@@ -1,7 +1,7 @@
 function dice (client,channel,tags,message) {
     const sides = 6;
     var diceRoll = Math.floor(Math.random() * sides) + 1;
-    client.say(channel, "@${tags.username}, you rolled a " + diceRoll)
+    client.say(channel, `@${tags.username}, you rolled a ` + diceRoll)
 }
 
 module.exports = { dice }
